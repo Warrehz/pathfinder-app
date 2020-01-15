cea8386582117a7d6006b67eba8ee4ff58d7d89e
unknown <mical.juarez@gmail.com>
2020-01-14 23:24:35 -0600

    have a working ability scores section that needs improvement

--- src/app/pathfinder-2e/ability-score-item/ability-score-item.component.ts
+++ src/app/pathfinder-2e/ability-score-item/ability-score-item.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AbilityScoreItemComponent implements OnInit {
 
+  scores = [
+    {name: 'STR',
+     score: 9},
+    {name: 'DEX',
+     score: 14},
+    {name: 'CON',
+     score: 16},
+    {name: 'INT',
+     score: 18},
+    {name: 'WIS',
+     score: 10},
+    {name: 'CHA',
+     score: 12}
+  ]
+
+  findMod = (ability) => {
+    return Math.floor((ability.score - 10) / 2);
+  }
+
   constructor() { }
 
   ngOnInit() {
